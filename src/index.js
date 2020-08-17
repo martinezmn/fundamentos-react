e96d6814ba518a56cb17c17e9addf5428663b77c
Martinez <gabrielmn378@gmail.com>
2020-08-17 16:40:46 -0300

    using component props

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
-    <div><Primeiro></Primeiro></div>,
+    <div>
+        <Primeiro></Primeiro>
+        <ComParametro
+            titulo="Situação do aluno"
+            aluno="Gabriel"
+            nota={9.2} />
+    </div>,
     document.getElementById('root')
 )
