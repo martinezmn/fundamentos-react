2bf0bf3813c8c34cceabe37670fa32a98f298221
Martinez <gabrielmn378@gmail.com>
2020-08-18 22:03:51 -0300

    fragmentos

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 ReactDOM.render(
     <div>
@@ -12,6 +13,7 @@ ReactDOM.render(
             titulo="Situação do aluno"
             aluno="Gabriel"
             nota={9.2} />
-    </div>,
+        <Fragmento/>
+    </div>, 
     document.getElementById('root')
 )
