26733c05becfb5ea841e38b8b01f1f5a3f433c7b
Martinez <gabrielmn378@gmail.com>
2020-08-21 10:57:31 -0300

    create family and childrens

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,22 @@ import ComParametro from "./components/basicos/ComParametro"
 import Fragmento from "./components/basicos/Fragmento"
 import Aleatorio from "./components/basicos/Aleatorio"
 import Card from "./components/layout/Card"
+import Familia from "./components/basicos/Familia"
+import FamiliaMembro from "./components/basicos/FamiliaMembro"
 
 export default props =>
     <div className="app">
         <h1>Fundamento React</h1>
 
         <div className="cards">
+            <Card titulo="Componente com Filhos" color="#00C8F8">
+                <Familia sobrenome="Ferreira">
+                    <FamiliaMembro nome="Pedro" />
+                    <FamiliaMembro nome="João" />
+                    <FamiliaMembro nome="Gabriel" />
+                </Familia>
+            </Card>
+
             <Card titulo="Número aleatório" color="#FA6900">
                 <Aleatorio min={500} max={100} />
             </Card>
