9b71615d9fd8c9e3ea353f4d6b3b7af466a6b60a
Martinez <gabrielmn378@gmail.com>
2020-08-21 10:14:20 -0300

    style cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import './app.css'
 
 import Primeiro from "./components/basicos/Primeiro"
 import ComParametro from "./components/basicos/ComParametro"
@@ -7,25 +8,27 @@ import Aleatorio from "./components/basicos/Aleatorio"
 import Card from "./components/layout/Card"
 
 export default props =>
-    <div id="app">
+    <div className="app">
         <h1>Fundamento React</h1>
-        <Card titulo="Número aleatório">
-            <Aleatorio min={500} max={100} />
-        </Card>
 
-        <Card titulo="Fragmento">
-            <Fragmento />
-        </Card>
+        <div className="cards">
+            <Card titulo="Número aleatório" color="#FA6900">
+                <Aleatorio min={500} max={100} />
+            </Card>
 
-        <Card titulo="Com parâmetro">
-            <ComParametro
-                titulo="Situação do aluno"
-                aluno="Gabriel"
-                nota={9.2} />
-        </Card>
+            <Card titulo="Fragmento" color="#E94C6F">
+                <Fragmento />
+            </Card>
 
-        <Card titulo="Primeiro">
-            <Primeiro></Primeiro>
-        </Card>
+            <Card titulo="Com parâmetro" color="#E8B71A">
+                <ComParametro
+                    titulo="Situação do aluno"
+                    aluno="Gabriel"
+                    nota={9.3} />
+            </Card>
 
+            <Card titulo="Primeiro" color="#588C73">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
     </div>
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -2,9 +2,15 @@ import "./Card.css"
 import React from "react"
 
 export default props => {
-    const { titulo } = props
+    const {titulo, color} = props
+
+    const cardStyle = {
+        backgroundColor: color || '#F00',
+        borderColor: color || '#F00'
+    }
+    
     return (
-        <div className="card">
+        <div className="card" style={cardStyle}>
             <div className="title">{titulo}</div>
             <div className="content">
                 {props.children}
